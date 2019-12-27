5db4dc26e9731633cf9728d77b36a7f456e9d599
Rohan Battula <rohanbattula@ucla.edu>
2019-12-27 12:28:47 -0800

    Initial Commit

---
+++ RecieptCalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  RecieptCalculator
+//
+//  Created by Rohan Battula on 12/27/19.
+//  Copyright © 2019 Rohan Battula. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
